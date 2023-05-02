0996fc8227921e2e347076b9a5c01abd6e068550
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-05-01 20:43:08 -0600

    Update Inhertaince_Assignment.py

--- Inhertaince_Assignment.py
+++ Inhertaince_Assignment.py
@@ -3,9 +3,9 @@ class PlayerCharacter: #create class Player character
     HP = 10 #Health points of the character
 
 class Warrior(PlayerCharacter): #create child class Warrior
-    weaponSpecialty: 'Sword' #Feature of warrior class, weapon specialty
-    armorClass: 2 #Feature of warrior class, how tough are they?
+    weaponSpecialty= 'Sword' #Feature of warrior class, weapon specialty
+    armorClass= 2 #Feature of warrior class, how tough are they?
 
 class Mage(PlayerCharacter): #create child class Mage
-    signitureSpell: 'Fireball' #Feature of Mage class, signature spell attack
-    magicDefense: 2 #Feature of Mage class, defense against magic attacks
+    signitureSpell= 'Fireball' #Feature of Mage class, signature spell attack
+    magicDefense=2 #Feature of Mage class, defense against magic attacks
